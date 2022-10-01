a2000141d3da4096253296a6f5e3445af6e31115
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-01 20:18:19 +0530

    Update HelloWorldSaksham.cpp

--- HelloWorldSaksham.cpp
+++ HelloWorldSaksham.cpp
@@ -2,21 +2,32 @@
 
 using namespace std;
 
+#define ll long long
+#define ld long double
+#define pll pair<ll,ll>
+#define vl vector<ll>
+#define vvl vector<vector<ll>>
+#define pb push_back
+#define PI acos(-1)
+#define cld complex<ld>
+#define fix(f,n) fixed<<setprecision(n)<<f
+#define all(x) x.begin(),x.end()
+#define popcount(x) __builtin_popcountll(x)
+#define sz(x) ((ll)x.size())
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll begtime = clock();
+#define end_routine() cerr << "\n\nTime elapsed: " << (clock() - begtime)*1000/CLOCKS_PER_SEC << " ms\n\n";
+
 void OJ() {
 #ifndef ONLINE_JUDGE
-
   freopen("input.txt", "r", stdin);
   freopen("output.txt", "w", stdout);
-
 #endif
 }
-
 int main() {
   OJ();
   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-
-
+  // code goes here.
   return 0;
-
 }
 
