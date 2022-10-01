3409aa0380ad5415e812f9ea49ca20318a946b1d
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-01 19:56:35 +0530

    Create HelloWorldSaksham.cpp

---
+++ HelloWorldSaksham.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void OJ() {
+#ifndef ONLINE_JUDGE
+
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+
+#endif
+}
+
+int main() {
+  OJ();
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+
+  return 0;
+
+}
+
