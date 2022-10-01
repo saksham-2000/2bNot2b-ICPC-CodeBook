5421e178dfb274cf50474c7502e35da22adfc43a
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-01 20:45:29 +0530

    Create SieveTotientSaksham.cpp

---
+++ SieveTotientSaksham.cpp
@@ -0,0 +1,22 @@
+const ll N = 2e5 + 5;
+ll phi[N],sp[N];
+void fill_totient(ll N) {
+  for (ll i = 2; i < N; i++)phi[i] = i;
+    for (ll i = 2; i < N; i++) {
+      if (phi[i] == i) {
+        for (ll j = i; j < N; j += i) {
+          phi[j] -= phi[j] / i;
+        }
+      }
+    }
+  }
+  void fill_sieve(ll N) {
+    for (ll i = 1; i < N; i++)sp[i] = i;
+      for (ll i = 2; i < N; i++) {
+        if (sp[i] == i) {
+          for (ll j = i * i; j < N; j += i) {
+            if (sp[j] == j)sp[j] = i;
+          }
+        }
+      }
+    }
