0b3ef1e6215cf8df5ef70535cb3e09a7b484cb59
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-01 19:49:10 +0530

    Update ConvexHullSaksham.cpp

--- ConvexHullSaksham.cpp
+++ ConvexHullSaksham.cpp
@@ -1,19 +1,16 @@
 struct seg{ ld x; ll m,b;};
- 
-vector<seg>hull;
- 
+ vector<seg>hull;     // for maxima, lines to be inserted in non-dec order, for minima,  non inc.
 void insert_to_hull(ll m,ll b){
   while(hull.size()){
     seg s=hull.back();
-    if(s.b+s.m*s.x>b+m*s.x){
+    if(s.b+s.m*s.x>b+m*s.x){  // for minimum '<' sign will be there.
       if(s.m-m)hull.push_back({(b-s.b)/(ld)(s.m-m),m,b});
       return;
     }
-    hull.pop_back();
+    hull.pop_back(); 
   }
   hull={{LLONG_MIN,m,b}};
 }
- 
 ll query_hull(ll x){
   seg s=*--upper_bound(hull.begin(),hull.end(),x,[](ll a,seg b){return a<b.x;});
   return s.b+s.m*x;
