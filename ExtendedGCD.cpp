75a5ade5074ba8a666e8b6e921439be423b7f948
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-02 18:41:14 +0530

    Update ExtendedGCD.cpp

--- ExtendedGCD.cpp
+++ ExtendedGCD.cpp
@@ -5,7 +5,8 @@ ll extended_gcd(ll a, ll b, ll& x, ll& y) {
     if (b == 0) {
         x = 1,y = 0;
         return a;
-    } // finds one (x,y) s.t ax+by = gcd(a,b) 
+    } // finds one (xo,yo) s.t ax+by = gcd(a,b) 
+    // general solution x=xo+k*(b/g),y=yo-k*(a/g) 
     ll x1, y1;
     ll d = extended_gcd(b, a % b, x1, y1);
     x = y1,y = x1 - y1 * (a / b);
