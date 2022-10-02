44f9c175c39b5a75f8bb3503cfa8c3f58e20a767
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-02 17:39:46 +0530

    Update PrimeListNTT.cpp

--- PrimeListNTT.cpp
+++ PrimeListNTT.cpp
@@ -1,3 +1,4 @@
  // Prime: 998244353 = 119 * 2^23 + 1, Primitive root: 3
 //  Prime: 985661441 = 235 * 2^22 + 1, Primitive root: 3
-//  Prime: 1012924417 = 483 * 2^21 + 1. Primitive root: 5
+//  Prime: 1012924417 = 483 * 2^21 + 1, Primitive root: 5
+//  Prime: 7340033 = 7* 2^21 +1, Primitive root: 5
