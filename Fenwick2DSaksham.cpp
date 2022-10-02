c84c0107c82138e99db72fc30190cd15e3b6c2f3
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-02 18:28:23 +0530

    Update Fenwick2DSaksham.cpp

--- Fenwick2DSaksham.cpp
+++ Fenwick2DSaksham.cpp
@@ -1,32 +1,18 @@
-struct Fenwick2D{
-  vector<vector<ll>> data;
-  ll sizeX, sizeY; // 1 Based-Indexing
-  Fenwick2D(ll sizeX, ll sizeY){
-    this->sizeX = sizeX,this->sizeY = sizeY;
-    data.resize(sizeX + 1);
-    for(ll i = 0; i <= sizeX; ++i){
-      data[i].resize(sizeY);
-    }
+struct Fenwick2D {
+  vector<vector<int>> bit;
+  int n, m; // 1 Based indexing
+  Fenwick2D(int n,int m){
+    this->n=n,this->m=m;
+      bit.assign(n+1,vector<int>(m+1,0));
   }
-  void update(ll x, ll y, ll val){
-    while(x <= sizeX){
-      ll y1 = y;
-      while(y <= sizeY){
-        data[x][y] += val,y += y & -y;
-      }
-      y = y1,x += x & -x;
-    }
+  int sum(int x, int y) {
+    int ret = 0;
+    for (int i = x; i > 0; i = (i & (i + 1)) - 1)
+      for (int j = y; j > 0; j = (j & (j + 1)) - 1) ret += bit[i][j];
+    return ret;
   }
-  ll get(ll x, ll y){
-    ll result = 0;
-    while(x > 0){
-      ll y1 = y;
-      while(y > 0){
-        result += data[x][y];
-        y -= y & -y;
-      }
-      x -= x & -x,y = y1;
-    }
-    return result;
+  void update(int x, int y, int delta) {
+    for (int i = x; i <=n; i = i | (i + 1))
+      for (int j = y; j <=m; j = j | (j + 1)) bit[i][j] += delta;
   }
 };
