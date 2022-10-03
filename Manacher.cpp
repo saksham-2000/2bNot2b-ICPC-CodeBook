fab0824faca1494b32f0457833960f71bf6a724c
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-03 15:21:03 +0530

    Create Manacher.cpp

---
+++ Manacher.cpp
@@ -0,0 +1,29 @@
+// If the size of palindrome centered at i is 'x' , then res stores (x+1)/2 
+ vector<int> manacher_odd(string s) {
+    int n = s.size();
+    s = "$" + s + "^";
+    vector<int> p(n + 2);
+    int l = 1, r = 1;
+    for(int i = 1; i <= n; i++) {
+        p[i] = max(0, min(r - i, p[l + (r - i)]));
+        while(s[i - p[i]] == s[i + p[i]]) {
+            p[i]++;
+        }
+        if(i + p[i] > r) {
+            l = i - p[i], r = i + p[i];
+        }
+    }
+    return vector<int>(begin(p) + 1, end(p) - 1);
+}
+
+// res[2i] and res[2i+1] are lengths of  
+// largest odd & even len. palindromes centered at i resp.
+vector<int> manacher(string s) {
+    string t;
+    for(auto c: s) {
+        t += string("#") + c;
+    }
+    auto res = manacher_odd(t + "#");
+    for(auto& d:res)d-=1;
+    return vector<int>(begin(res) + 1, end(res) - 1);
+}
