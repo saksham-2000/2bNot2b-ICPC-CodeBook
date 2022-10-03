517d95030039582c4382659d9517eca5df62fb5a
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-03 15:45:19 +0530

    Update CRT.cpp

--- CRT.cpp
+++ CRT.cpp
@@ -2,7 +2,7 @@ for (int i = 0; i < k; ++i) {
     x[i] = a[i];
     for (int j = 0; j < i; ++j) {
         x[i] = r[j][i] * (x[i] - x[j]);
-
+// r[i][j] is (p[i])^-1 modulo p[j]
         x[i] = x[i] % p[i];
         if (x[i] < 0)
             x[i] += p[i];
