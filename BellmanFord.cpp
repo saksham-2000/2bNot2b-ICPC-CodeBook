7b543aceeb9f997ec2553ecfade242b55afec706
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-05 14:15:40 +0530

    Create BellmanFord.cpp

---
+++ BellmanFord.cpp
@@ -0,0 +1,17 @@
+// find shortest dist. from v to all other vertices.
+void solve(){
+    vector<int> d (n, INF);
+    d[v] = 0;
+    while(1){
+        bool any = false;
+        for (int j=0; j<m; ++j){
+         if (d[e[j].a] >= INF)continue; 
+  // to avoid neg. edge weight cases.
+         if (d[e[j].b] > d[e[j].a] + e[j].cost){
+          d[e[j].b] = d[e[j].a] + e[j].cost;
+            any = true;
+          }
+        }
+      if (!any) break;
+    }
+}
