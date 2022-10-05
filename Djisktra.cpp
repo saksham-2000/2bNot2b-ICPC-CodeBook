7a4b431a6c63bf46993ac4e0244c62c410dd49be
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-05 14:06:17 +0530

    Update Djisktra.cpp

--- Djisktra.cpp
+++ Djisktra.cpp
@@ -1,22 +1,10 @@
-const int INF = 1000000000;
-vector<vector<pair<int, int>>> adj;
-
-void dijkstra(int s, vector<int> & d, vector<int> & p) {
-    int n = adj.size();
-    d.assign(n, INF);
-    p.assign(n, -1);
-
-    d[s] = 0;
-    set<pair<int, int>> q;
-    q.insert({0, s});
+// Fails on neg. edge-weights & neg. cycles.
     while (!q.empty()) {
         int v = q.begin()->second;
         q.erase(q.begin());
-
         for (auto edge : adj[v]) {
             int to = edge.first;
             int len = edge.second;
-
             if (d[v] + len < d[to]) {
                 q.erase({d[to], to});
                 d[to] = d[v] + len;
