67f1ab5777529590d744e657e1371d95db5f35e0
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2022-10-05 13:49:34 +0530

    Create WA-TLE.cpp

---
+++ WA-TLE.cpp
@@ -0,0 +1,13 @@
+1) sqrt(): precision issues, so check for x-1,x+1 or use sqrtl() instead
+2) check corner/base cases
+3) write slow/brute soln for patterns/observations.
+4) declare array/vec's globally 
+5) check for conflicting var names specially in for loops
+6) Do not erase from set while iterating it
+7) Read prob carefully,is something unique,sorted,adjacent,guaranteed??
+8) Dont use if(!mp[x]) if you want to iterate map later
+9) Initialise everything
+10) Check bounds in binary search
+11) Can a variable you are dividing by be 0?
+12) if multiplce tcs, clear vectors,sets,maps etc
+
