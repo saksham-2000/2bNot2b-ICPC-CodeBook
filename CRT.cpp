2ac4eacbee3b6afc32f632947c03e4a6bfd03122
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2023-01-22 15:20:32 +0530

    Update CRT.cpp

--- CRT.cpp
+++ CRT.cpp
@@ -8,5 +8,5 @@ for (int i = 0; i < k; ++i) {
             x[i] += p[i];
     }
 }
-// if A is ai modulo (pi) (i from 0,k-1)
+// if A is ai modulo (pi) (i from 0,k-1). (Here, pi's are pairwise coprime, not necessarily prime)
 // then A = summation(xi*prod(i-1)) where prod(j) is p0*(p1)*......(pj) 
