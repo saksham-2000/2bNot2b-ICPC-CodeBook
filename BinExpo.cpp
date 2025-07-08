6a86a74ba2d643467ec23bc5464733ce128dcba5
Saksham Garg <91961606+saksham-2000@users.noreply.github.com>
2025-07-08 13:00:50 +0530

    Update and rename BinExpFermPrime.cpp to BinExpo.cpp

--- BinExpo.cpp
+++ BinExpo.cpp
@@ -1,3 +1,5 @@
+long long MOD = 1e9+7;
+
 ll poww(ll a, ll b){  // BINARY EXPO
   if(b<0)return 0;
   ll ans = 1;
@@ -37,16 +39,4 @@ ll powMbig(ll a, ll b, ll m){
     a = mulmod(a, a, m),b >>= 1;
   }
   return ans; // (a^b) %m for m >1e9 (to avoid overflow) 
-}
-// Primality test in logN time (non-deterministic)
-bool fermatprime(ll x) {
-  if (x <= 3)return (x == 2 || x == 3);
-  for (ll iter = 0; iter < 20; iter++) {
-    ll a = rng() % (x - 3) + 2; // random base in [2,x-2]
-    ll temp = powMbig(a, x - 1, x);
-    if (temp != 1) {
-      return false;
-    }
-  } // Beware ! Can lead to 'psuedo' primes.
-  return true;
 }
